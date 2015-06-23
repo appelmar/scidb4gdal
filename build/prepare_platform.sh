d298176b8e227472eb154e127ed322777cee5f93
Marius Appel <mappl@users.noreply.github.com>
2015-06-23 15:23:02 +0200

    Update prepare_platform.sh

--- build/prepare_platform.sh
+++ build/prepare_platform.sh
@@ -10,11 +10,11 @@ wget http://download.osgeo.org/gdal/1.11.2/gdal-1.11.2.tar.gz
 tar -xf gdal-1.11.2.tar.gz
 
 # 3. add scidb driver to GDAL source; given line numbers are very likely to change with GDAL versions!
-sed -i "37i void CPL_DLL GDALRegister_SciDB(void);" gcore/gdal_frmts.h
-sed -i "78i #ifdef FRMT_scidb" frmts/gdalallregister.cpp
-sed -i "79i GDALRegister_SciDB();" frmts/gdalallregister.cpp
-sed -i "80i #endif" frmts/gdalallregister.cpp
-sed -i "484i scidb \\\\" GDALmake.opt.in
+sed -i "37i void CPL_DLL GDALRegister_SciDB(void);" gdal-1.11.2/gcore/gdal_frmts.h
+sed -i "78i #ifdef FRMT_scidb" gdal-1.11.2/frmts/gdalallregister.cpp
+sed -i "79i GDALRegister_SciDB();" gdal-1.11.2/frmts/gdalallregister.cpp
+sed -i "80i #endif" gdal-1.11.2/frmts/gdalallregister.cpp
+sed -i "484i scidb \\\\" gdal-1.11.2/GDALmake.opt.in
 
 # 4. copy scidb driver sources to gdal sources
-cp -R src build/gdal-1.11.2/scidb
+cp -R src gdal-1.11.2/frmts/scidb
