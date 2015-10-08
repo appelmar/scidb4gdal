377d25b9735ae2593d1359cf0814f7366e990b94
Marius Appel <mappl@users.noreply.github.com>
2015-10-08 16:58:29 +0200

    Fixed line numbers in prepare_platform.sh

--- build/prepare_platform.sh
+++ build/prepare_platform.sh
@@ -11,10 +11,10 @@ tar -xf gdal-2.0.1.tar.gz
 
 # 3. add scidb driver to GDAL source; given line numbers are very likely to change with GDAL versions!
 sed -i "37i void CPL_DLL GDALRegister_SciDB(void);" gdal-2.0.1/gcore/gdal_frmts.h
-sed -i "78i #ifdef FRMT_scidb" gdal-2.0.1/frmts/gdalallregister.cpp
-sed -i "79i GDALRegister_SciDB();" gdal-2.0.1/frmts/gdalallregister.cpp
-sed -i "80i #endif" gdal-2.0.1/frmts/gdalallregister.cpp
-sed -i "484i scidb \\\\" gdal-2.0.1/GDALmake.opt.in
+sed -i "62i #ifdef FRMT_scidb" gdal-2.0.1/frmts/gdalallregister.cpp
+sed -i "63i GDALRegister_SciDB();" gdal-2.0.1/frmts/gdalallregister.cpp
+sed -i "64i #endif" gdal-2.0.1/frmts/gdalallregister.cpp
+sed -i "498i scidb \\\\" gdal-2.0.1/GDALmake.opt.in
 
 # 4. copy scidb driver sources to gdal sources
 cp -R src gdal-2.0.1/frmts/scidb
